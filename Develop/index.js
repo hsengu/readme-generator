189a0a255d48e47b29200c2b506563612c1fa6fa
Hok S. Uy <hoksuy@gmail.com>
2021-08-15 10:42:21 -0700

    moved node package json files to Develop folder, added inquirer code

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,92 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your project name? (Required) : ",
-                   "Enter a description for your project (Required) : ",
-                   "Enter installation instructions for your project (Required)",
-                   "Enter usage information for your project (Required)",
-                   "Enter contribution information for your project (Required)",
-                   "Enter test information for your project (Required)",
-                   "Does your project have a license?",
-                   "Which license does your project use?"
+const questions = [{
+                     type: 'input',
+                     name: 'name',
+                     message: 'What is your project name? (Required) : ',
+                     validate: nameInput => {
+                        if(!nameInput)
+                           console.log('You must enter a project name!');
+                        return nameInput !== '';
+                     }
+                   },
+                   {
+                     type: 'input',
+                     name: 'description',
+                     message: 'Enter a description for your project (Required) : ',
+                     validate: descriptionInput => {
+                        if(!descriptionInput)
+                           console.log('You must enter a description!');
+                        return descriptionInput !== '';
+                     }
+                   },
+                   {
+                     type: 'input',
+                     name: 'instructions',
+                     message: 'Enter installation instructions for your project (Required)',
+                     validate: instructionsInput => {
+                        if(!instructionsInput)
+                           console.log('You must enter installation instructions!');
+                        return instructionsInput !== '';
+                     }
+                   },
+                   {
+                     type: 'input',
+                     name: 'usage',
+                     message: 'Enter usage information for your project (Required)',
+                     validate: usageInput => {
+                        if(!usageInput)
+                           console.log('You must enter usage instructions!');
+                        return usageInput !== '';
+                     }
+                   },
+                   {
+                     type: 'input',
+                     name: 'contribution',
+                     message: 'Enter contribution information for your project (Required)',
+                     validate: contributionInput => {
+                        if(!contributionInput)
+                           console.log('You must enter contribution instructions!');
+                        return contributionInput  !== '';
+                     }
+                   },
+                   {
+                     type: 'input',
+                     name: 'test',
+                     message: "Enter test information for your project (Required)",
+                     validate: testInput => {
+                        if(!testInput)
+                           console.log('You must enter test information!');
+                        return testInput  !== '';
+                     }
+                   },
+                   {
+                     type: 'confirm',
+                     name: 'hasLicense',
+                     message: 'Does your project have a license?',
+                     default: true
+                   },
+                   {
+                     type: 'list',
+                     name: 'license',
+                     message: 'Which license does your project use?',
+                     choices: ['Apache License 2.0', 'GNU GPL v2', 'GNU GPL v3', 'ISC License', 'MIT License'],
+                     when: ({hasLicense}) => hasLicense
+                   }
                   ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-   fs.writeFile("README.md", generateMarkdown(data));
+   fs.writeFile(fileName, generateMarkdown(data));
 }
 
 // TODO: Create a function to initialize app
 function init() {
-   inquirer.prompt([
-      
-   ]);
+   inquirer.prompt(questions).then(answers => console.log(answers));
 }
 
 // Function call to initialize app
