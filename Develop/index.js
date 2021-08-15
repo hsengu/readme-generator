a44e5f45e40e1915f8f71e7691dddf7ed04f11a3
Hok S. Uy <hoksuy@gmail.com>
2021-08-15 10:07:43 -0700

    Added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project name? (Required) : ",
+                   "Enter a description for your project (Required) : ",
+                   "Enter installation instructions for your project (Required)",
+                   "Enter usage information for your project (Required)",
+                   "Enter contribution information for your project (Required)",
+                   "Enter test information for your project (Required)",
+                   "Does your project have a license?",
+                   "Which license does your project use?"
+                  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+   fs.writeFile("README.md", generateMarkdown(data));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   inquirer.prompt([
+      
+   ]);
+}
 
 // Function call to initialize app
 init();
